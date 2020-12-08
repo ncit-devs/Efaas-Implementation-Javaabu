69b8115b3b3d175ad1f08cb5cf75c3c5003438ec
Mohamed Jailam <muhammed.jailam@gmail.com>
2020-12-08 15:24:49 +0500

    - Added Login Page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
