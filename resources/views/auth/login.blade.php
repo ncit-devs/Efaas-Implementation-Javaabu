a20ca87c59c90430ba044eefde889f093d366fd9
Mohamed Jailam <muhammed.jailam@gmail.com>
2020-12-08 15:34:38 +0500

    - Updated Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,13 @@
 <x-guest-layout>
+    <section class="relative flex flex-col items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
 
-<section class="relative flex flex-col items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <img src="{{ asset('/img/eflogo.png') }}" class="w-25 mb-12" alt="EFaas">
 
-    <img src="{{ asset('/img/eflogo.png') }}" class="w-25 mb-12" alt="EFaas">
-
-    <a href="#" class="hover:bg-gray-500 uppercase bg-transparent hover:bg-blue text-blue-dark font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded">
-        Click to login wih EFaas
-    </a>
-</section>
-
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <button href="#" class="hover:bg-gray-500 uppercase bg-transparent hover:bg-blue text-blue-dark font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded">
+                Click to login wih EFaas
+            </button>
+        </form>
+    </section>
 </x-guest-layout>
